75348457e612c30d1e3d3088aab9f5ca135139b4
alex-be2 <149081899+alex-be2@users.noreply.github.com>
2025-11-26 15:53:21 +0000

    Add files via upload

--- ABCComplaintManagementSystem/ABCComplaintManagementSystem/obj/Debug/net8.0/ABCComplaintManagementSystem.AssemblyInfo.cs
+++ ABCComplaintManagementSystem/ABCComplaintManagementSystem/obj/Debug/net8.0/ABCComplaintManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCComplaintManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e01f697e82b2e178ef88ba8c0b4ea9a706fe646")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCComplaintManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCComplaintManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
